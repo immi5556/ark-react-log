7027ecf7b3fc68130a5620e54297beb7fb3c7565
jiraj <immanuelraj.j@clariant.com>
2021-05-02 01:43:31 +0530

    Updates

---
+++ precache-manifest.52284f949614fcc8d43818032bd24eca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609c901e0e3eca1d486b86a41fb637ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "934e370820391e3183ac",
+    "url": "./static/css/main.4e6b4825.chunk.css"
+  },
+  {
+    "revision": "34728e8c1c4d2dd6bdae",
+    "url": "./static/js/2.d802b488.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d802b488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934e370820391e3183ac",
+    "url": "./static/js/main.6870479f.chunk.js"
+  },
+  {
+    "revision": "5540fdca3e63c6da180d",
+    "url": "./static/js/runtime-main.2aba8468.js"
+  },
+  {
+    "revision": "88401a364bd98b3cd80f3d16ef1f770a",
+    "url": "./static/media/csle_1.88401a36.PNG"
+  },
+  {
+    "revision": "24313229786a0b2a79b491b82c106ad4",
+    "url": "./static/media/csle_err_with_tag.24313229.PNG"
+  },
+  {
+    "revision": "56068a71ef854b6f4344c166c79ed923",
+    "url": "./static/media/csle_log_default_tag.56068a71.PNG"
+  },
+  {
+    "revision": "f47569aa831a11bbc725fab208fcab0c",
+    "url": "./static/media/csle_log_with_tag.f47569aa.PNG"
+  },
+  {
+    "revision": "f7cda69be3fe8d8ede4b076e91d5c219",
+    "url": "./static/media/react_dev.f7cda69b.png"
+  }
+]);
